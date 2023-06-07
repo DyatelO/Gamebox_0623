5e0836dd4ad97d04db2cf0d44400b6f5de8ca5a4
DyatelO <ishitory89@yandex.ru>
2023-06-07 19:19:45 +0300

    Незначительные изменения.

---
+++ Merge-2/Assets/Scriprs/Items/ItemBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Items
+{
+    public class ItemBase 
+    {
+        public Image ItemView;
+        //public ItemData
+    }
+}
+
--- Merge-2/Assets/Scriprs/MainField/Board.cs
+++ Merge-2/Assets/Scriprs/MainField/Board.cs
@@ -1,16 +1,20 @@
+//using Items;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Board : MonoBehaviour
 {
+   // private ItemBase itembase = new ItemBase();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //itembase 
     }
 
-    // Update is called once per frame
+    // Update is called once per frames
     void Update()
     {
         
