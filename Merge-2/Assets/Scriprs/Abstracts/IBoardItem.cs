64e5232d67d6feccd1f6b682a6c8ff501e4147c4
DyatelO <ishitory89@yandex.ru>
2023-06-08 19:55:06 +0300

    Мелкие правки.

---
+++ Merge-2/Assets/Scriprs/Abstracts/IBoardItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBoardItem 
+{
+    bool IsFree { get; set; }
+    Type ItemType { get; }
+    int MaxItemsInSlot { get; }
+    int AmountItems { get; set; }
+
+    IBoardItem Clone();
+}
---
+++ Merge-2/Assets/Scriprs/Abstracts/IBoardSlot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IBoard
+{
+    int Copacity { get; set; }
+    bool isFull { get; set; }
+
+    IBoardItem GetItem(Type itemType);
+    IBoardItem[] GetAllItems();
+    IBoardItem[] GetAllItems(Type itemType);
+    IBoardItem[] GetEquippedItems();
+    int GetItemsAmount(Type itemType);
+
+    bool TryAdd(object sendler, IBoardItem item);
+    bool Remove(object sendler, Type itemType, int amount = 1);
+    bool HAsItem(Type itemType, out IBoardItem item);
+
+}
+
+namespace Assets.Scriprs.Abstracts
+{
+    public interface IBoardSlot
+    {
+        bool isFull { get; }
+        bool isEmpty { get; }
+        IBoardItem Item { get; }
+        Type ItemType { get; }
+        int AmountItems { get; }
+        int Copacity { get; }
+
+        void SetItem(IBoardItem item);
+        void Clear();
+    }
+}
