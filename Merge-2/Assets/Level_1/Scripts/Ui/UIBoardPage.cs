195327433bfdfc86d02b3d9b890da4fedda0a1e3
DyatelO <ishitory89@yandex.ru>
2023-06-11 18:45:16 +0300

    Внедряем ивенты.

--- Merge-2/Assets/Level_1/Scripts/Ui/UIBoardPage.cs
+++ Merge-2/Assets/Level_1/Scripts/Ui/UIBoardPage.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Graphs;
 using UnityEngine;
 
 public class UIBoardPage : MonoBehaviour
@@ -15,9 +17,35 @@ public class UIBoardPage : MonoBehaviour
         for (int i = 0; i < itemsAmount; i++)
         {
             UiBoardItem uiItem = Instantiate(itemPrefab, Vector2.zero, Quaternion.identity);
+            uiItem.name = $"{itemPrefab.name}_{i}";
             uiItem.transform.SetParent(contentPanel, false);
             listOfItems.Add(uiItem);
+
+            uiItem.OnBoardItemClicked += HaHandleItemSelection;
+            uiItem.OnBoardItemDroppedOn += HaHandleDroppedOn;
+            uiItem.OnBoardItemBeginDrag += HaHandleBeginDrag;
+            uiItem.OnBoardItemEndDrag += HaHandleEndDrag;
         }
+    }
+
+    private void HaHandleEndDrag(UiBoardItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HaHandleBeginDrag(UiBoardItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HaHandleDroppedOn(UiBoardItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HaHandleItemSelection(UiBoardItem obj)
+    {
+        Debug.Log(obj.name);
     }
 
     public void Show()
